06243f3f0015cff343e3e33d36bbe00545d473a7
twenty-five <twenty-five@SAMSUNG-SM-G930V.localdomain>
2022-09-07 16:49:08 -0400

    added boilerplate

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
-import { Canvas } from "@react-three/fiber";
 import css from "../styles/Home.module.css";
+import { Canvas } from "@react-three/fiber";
+import Box from "../components/Box";
+import OrbitControls from "../components/OrbitControls";
+import Light from "../components/Light";
+import Floor from "../components/Floor";
+import Draggable from "../components/Draggable";
+import {Suspense} from "react";
 
 export default function Home() {
   return (
-    <div className=" w-screen h-screen">
+    <div className="w-screen h-screen">
       <Canvas
         shadows={true}
         className="bg-[#000]"
@@ -11,7 +17,15 @@ export default function Home() {
           position: [-6, 7, 7],
         }}
       >
-
+        <ambientLight color={"white"} intensity={0.2} />
+        <Light position={[0, 3, 0]} />
+        <Draggable>
+        <Suspense fallback={null}>
+            <Box rotateX={3} rotateY={0.2} />
+        </Suspense>
+        </Draggable>
+        <OrbitControls />
+        <Floor position={[0, -1, 0]} />
       </Canvas>
     </div>
   );
